1fec230bce492638ee9e9597438db55e8de0fe54
NateEmerson <emerson.nate@gmail.com>
2024-10-02 09:02:58 -0700

    Scaffold basic pages and left nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,15 +12,57 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Introduction',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Overview', slug: 'overview' },
+						{ label: 'Quickstart', slug: 'quickstart' },
+						{ label: 'NATS Fundamentals', slug: 'fundamentals' },
+					],
+				},
+				{
+					label: 'Development',
+					items: [
+						{ label: 'Connecting', slug: 'development/connecting' },
+						{ label: 'Messaging', slug: 'development/messaging' },
+						{ label: 'Streaming', slug: 'development/streaming' },
+						{ label: 'Key-Value', slug: 'development/key-value' },
+						{ label: 'Object Store', slug: 'development/object-store' },
+						{ label: 'Services', slug: 'development/services' },
+						{ label: 'Security', slug: 'development/security' },
+					],
+				},
+				{
+					label: 'Architecture',
+					items: [
+						{ label: 'Application', slug: 'architecture/application' },
+						{ label: 'System', slug: 'architecture/system' },
+					],
+				},
+				{
+					label: 'Deployment',
+					items: [
+						{ label: 'Cloud', slug: 'deployment/cloud' },
+						{ label: 'Edge', slug: 'deployment/edge' },
+						{ label: 'Kuberenetes', slug: 'deployment/kubernetes' },
+						{ label: 'Containers', slug: 'deployment/containers' },
+						{ label: 'Bare Metal', slug: 'deployment/bare-metal' },
+					],
+				},
+				{
+					label: 'Operations',
+					items: [
+						{ label: 'Monitoring', slug: 'operations/monitoring' },
+						{ label: 'Security', slug: 'operations/security' },
+						{ label: 'Updates', slug: 'operations/updates' },
+						{ label: 'Disaster Recovery', slug: 'operations/disaster-recovery' },
 					],
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					items: [
+						{ label: 'Server Config', slug: 'reference/server-config' },
+						{ label: 'Protocols', slug: 'reference/protocols' },
+					],
 				},
 			],
 		}),
